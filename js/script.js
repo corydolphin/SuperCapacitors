3380f1a0e17abd6a3e099f8e0caee2981c914876
Cory Dolphin <wcdolphin@gmail.com>
2012-04-26 15:10:55 -0400

    basic hover implemented, need to fix parent child relations

--- js/script.js
+++ js/script.js
@@ -4,11 +4,31 @@
 $(document).ready(function(){
 
 
-console.log('working');
+	console.log('working');
+	$('.infographicElement').hover(
+		function(){
+	 		$(this).children('.triggerElement').each(function(){
+	 			this.style.opacity = 0.9;
+	 		});
+		},
+	    function(){
+			$(this).children('.triggerElement').each(function(){
+	 			this.style.opacity = 0.2;
+	 		});
+		}
+	);
+	 		
 
+	$('.triggerElement').hover(
+		function(){
 
+	 	$('div#.callout').show();
+	 	$(this).parent().children('.callout').each(function(){});
+		},
+	    function(){
+	 	$('div#.callout').hide();
 
-
+	})
 });
 
 
