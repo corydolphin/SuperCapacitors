1bb9a2912338b3adc2ea8fa1bcf81dad9673d958
Cory Dolphin <wcdolphin@gmail.com>
2012-05-03 13:42:30 -0400

    added modal content via ajax

--- js/script.js
+++ js/script.js
@@ -3,42 +3,47 @@
 */
 $(document).ready(function(){
 
-	$('.btn').qtip();
-
 	$('#nowButton').bind('click',function(){$(this).parent().children().removeClass('active'); $(this).addClass('active');});
 	$('#nearButton').bind('click',function(){$(this).parent().children().removeClass('active'); $(this).addClass('active');});
 	$('#farButton').bind('click',function(){$(this).parent().children().removeClass('active'); $(this).addClass('active');});
-	console.log('working');
 
-
-	
-	$('.infographicElement').hover(
-		function(){
+	$('.infographicElement').hoverIntent(
+	{
+		sensitivity: 7,
+		interval: 100,
+		over: function(){
 	 		$(this).children('.calloutContainer').children('.triggerElement').each(function(){
 	 			$(this).addClass('active');	
 	 			
 	 		});
 		},
-	    function(){
+		timeout:1000,
+	    out: function(){
 	 		$(this).children('.calloutContainer').children('.triggerElement').each(function(){
 	 			
 	 			$(this).removeClass('active');
 	 		});
 		}
+	}
 	);
 	 
-	$('.triggerElement').hover(
-		function(){
+	$('.triggerElement').hoverIntent(
+	{
+		sensitivity: 7,
+		interval: 100,
+		over:function(){
 			$(this).parent().children('.callout').each(function(){
 				$(this).addClass('active');	
 			});
 
 		},
-	    function(){
+		timeout:1000,
+	    out:function(){
 			$(this).parent().children('.callout').each(function(){
 				$(this).removeClass('active');	
 	    	});	
 		}
+	}
 	);
 	$('.callout').hover(
 		function(){
@@ -48,6 +53,18 @@ $(document).ready(function(){
 			$(this).removeClass('active');	
 		}
 	);
+
+	modalGetter = function(id){
+		$.get('./modalContent/'+ id + '.html', function(html){
+			$('#' + String(id)).append(html);	
+		});
+	};
+	$('.modal.hide.in').each(function(){
+		
+		modalGetter(this.id);
+	});
+
+
 
 });
 
